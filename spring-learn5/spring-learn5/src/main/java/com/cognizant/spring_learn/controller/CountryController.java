82a32248a2a13737d9e3218f5670c9a609080137
M.S.Meenakshi <msmeenakshivoc@gmail.com>
2026-07-05 22:20:16 +0530

    Add files via upload
    Added Spring REST hands on 5

---
+++ spring-learn5/spring-learn5/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,39 @@
+package com.cognizant.spring_learn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        return countryService.getCountry("IN");
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        LOGGER.info("START");
+
+        Country country = countryService.getCountry(code);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+}
